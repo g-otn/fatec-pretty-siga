68003f80d7130c0fa8061ea3d7ce44b0efd41cad
g-otn <g_otn@hotmail.com>
2019-05-20 17:44:56 -0300

    Create client page pin icon and its style

--- public/scripts/menu.js
+++ public/scripts/menu.js
@@ -1,3 +1,12 @@
+function mostrarDiv(div) {
+    let abas = document.getElementById('abas')
+    for (aba in abas.children)
+        if (aba.id !== div)
+            aba.style.display = 'none'
+        else
+            aba.style.display = 'inline';
+}
+
 function sair() {
     window.onbeforeunload = null
     document.location.reload()
