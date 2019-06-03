9b1feefcedf9580fa280045c335d6defc424ecfd
g-otn <g_otn@hotmail.com>
2019-06-03 16:58:15 -0300

    Update design

--- routes/auth.js
+++ routes/auth.js
@@ -1,21 +1,20 @@
 var express = require('express')
 var app = module.exports = express()
-var siga = require('../util/siga')        // Padrão
-//var siga = require('../tests/mockSiga') // Para teste
+//var siga = require('../util/siga')        // Padrão
+var siga = require('../tests/mockSiga') // Para teste
 
 // Autenticação e envio de dados do aluno
 app.post('/', (req, res) => {
     siga.login(req.body.usuario, req.body.senha)
         .then(dadosAluno => {
-            console.log(dadosAluno)
             if (dadosAluno.erro !== undefined)
                 res.send(dadosAluno)
             else
                 res.render('main', dadosAluno, (erro, html) => {
-                    console.log(erro)
-                    if (erro)
+                    if (erro) {
+                        console.log(erro)
                         res.send({ erro: 'Ocorreu um erro no servidor e não foi possível gerar sua página de aluno' })
-                    else
+                    } else
                         res.send(html)
                 })
         })
